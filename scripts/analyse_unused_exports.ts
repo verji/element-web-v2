98203890a1d5a79ad2ebd0d6f844e2ad24aafc96
John Tore Simonsen <john.tore.simonsen@rosberg.com>
2025-03-11 16:03:15 +0100

    add matrix-react-sdk-module-api to ignore

--- scripts/analyse_unused_exports.ts
+++ scripts/analyse_unused_exports.ts
@@ -24,7 +24,7 @@ ignore.push("/SecurityManager.ts");
 ignore.push("/OpenReportEventDialogPayload.ts");
 ignore.push("/MiscHeaderButtons.tsx");
 ignore.push("/SearchBar.tsx");
-ignore.push("/matrix-react-sdk-module-api/lib/");
+ignore.push("matrix-react-sdk-module-api");
 
 // We ignore js-sdk by default as it may export for other non element-web projects
 if (!includeJSSDK) ignore.push("matrix-js-sdk");
