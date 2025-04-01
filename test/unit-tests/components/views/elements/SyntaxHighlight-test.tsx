edb8dec6d052cdbc31681ff90923e9f3dac37f6d
John Tore Simonsen <john.tore.simonsen@rosberg.com>
2025-04-01 16:10:38 +0200

    eslint

--- test/unit-tests/components/views/elements/SyntaxHighlight-test.tsx
+++ test/unit-tests/components/views/elements/SyntaxHighlight-test.tsx
@@ -12,7 +12,7 @@ import React from "react";
 import SyntaxHighlight from "../../../../../src/components/views/elements/SyntaxHighlight";
 
 // VERJI Skip - Should be looked into
-it.todo("Verji skip tests <SyntaxHighlight />")
+it.todo("Verji skip tests <SyntaxHighlight />");
 describe.skip("<SyntaxHighlight />", () => {
     it("renders", async () => {
         const { container } = render(<SyntaxHighlight>console.log("Hello, World!");</SyntaxHighlight>);
