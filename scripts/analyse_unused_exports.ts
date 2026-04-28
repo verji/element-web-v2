1042515a4e8fb1e25d88fc49a99ad2d4f371c2ca
John Tore Simonsen <john.tore.simonsen@rosberg.com>
2026-04-28 09:39:20 +0200

    update dependency to use matrix-react-sdk-module-api/verji-main-v2 specifically for this version

--- scripts/analyse_unused_exports.ts
+++ scripts/analyse_unused_exports.ts
@@ -26,6 +26,10 @@ ignore.push("/SearchBar.tsx");
 ignore.push("matrix-react-sdk-module-api");
 // VERJI - The following ignores are temporary and false-positives which need to be fixed
 ignore.push("/VerjiRoomHeaderOpts.tsx");
+// VERJI - default exports whose call sites in RoomView.tsx are currently commented out;
+// retained for re-enablement/upstream-port parity.
+ignore.push("/Searching.ts");
+ignore.push("/VerjiLocalSearch.ts");
 
 // We ignore js-sdk by default as it may export for other non element-web projects
 if (!includeJSSDK) ignore.push("matrix-js-sdk");
